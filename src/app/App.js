5f22b55503e8ca18fa98d5773f6caa99acc0be61
MattCornicDev <glgharib@gmail.com>
2026-08-19 17:43:43 +0200

    Ajout favicon
    Signed-off-by: MattCornicDev <glgharib@gmail.com>

--- src/app/App.js
+++ src/app/App.js
@@ -10,6 +10,9 @@ import Headermain from "../header";
 import AnimatedCursor from "../hooks/AnimatedCursor";
 import "./App.css";
 
+// 👉 Ajout Helmet
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 function _ScrollToTop(props) {
   const { pathname } = useLocation();
   useEffect(() => {
@@ -21,21 +24,29 @@ const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="cursor__dot">
-        <AnimatedCursor
-          innerSize={15}
-          outerSize={15}
-          color="255, 255 ,255"
-          outerAlpha={0.4}
-          innerScale={0.7}
-          outerScale={5}
-        />
-      </div>
-      <ScrollToTop>
-        <Headermain />
-        <AppRoutes />
-      </ScrollToTop>
-    </Router>
+    <HelmetProvider>
+      {/* 👉 Favicon global */}
+      <Helmet>
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="cursor__dot">
+          <AnimatedCursor
+            innerSize={15}
+            outerSize={15}
+            color="255, 255 ,255"
+            outerAlpha={0.4}
+            innerScale={0.7}
+            outerScale={5}
+          />
+        </div>
+
+        <ScrollToTop>
+          <Headermain />
+          <AppRoutes />
+        </ScrollToTop>
+      </Router>
+    </HelmetProvider>
   );
 }
